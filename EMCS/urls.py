83781fb19dbacd1888817d827939c4b0663de667
Iyad Elwy <iyadelwy1@gmail.com>
2022-10-25 15:40:36 +0200

    changed root routes

--- EMCS/urls.py
+++ EMCS/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('school.urls'))
+    path('api/v1/school', include('school.urls'))
 ]
